a00bcdff0fe379d6fbff49ac05aa2148b0288a91
ByeongUk-An <quddnr005@naver.com>
2022-03-03 22:40:25 +0900

    java object

--- src/chap05/Ex02.java
+++ src/chap05/Ex02.java
@@ -1,38 +1,41 @@
-package chap05;
+package Chap05;
 
 import java.util.Arrays;
 
 public class Ex02 {
 
 	public static void main(String[] args) {
+
 		int[] arr2 = new int[300];
-		for (int i = 0; i < arr2.length; i++) {
-			arr2[i] = (i + 1) * 3;
-
-			if (arr2[i] <= 300) {
-				System.out.println(arr2[i]);
-			}
+		// for 문을 사용해서 arr2 배열의 각방에 값을 입력 
+		
+		for ( int i = 0 ; i < arr2.length; i++) {		
+			arr2[i] = (i+1) *3 ;						
 		}
-
-		System.out.println();
-		for (int k : arr2) {
-
-			System.out.println(k);
-
+		
+		// 출력 구문 for 
+		for ( int i = 0 ; i < arr2.length; i++) {
+			System.out.println(i + "번째방의 값 : " + arr2[i]);
 		}
-
-		System.out.println();
-
+		System.out.println("================");
+		// 향상된(Enhanced ) for 문  
+		for (int g : arr2 ) {
+			System.out.println( g );
+		}
+		System.out.println("=====================");
+		
+		// Arrays.toString(arr1)
 		System.out.println(Arrays.toString(arr2));
-
-		for (int j = 0; j < arr2.length; j++) {
-//			System.out.println(arr2[j]);
-			if (arr2[j] % 2 == 1) {
-				if (j >= 100 && j <= 300) {
-					System.out.println(arr2[j]);
-				}
-			}
+		
+		
+		System.out.println("=========홀수 번의 방만 출력===============");
+		
+		for ( int i = 100 ; i < arr2.length; i++) {
+			if ( i%2 ==1)
+			System.out.println(i + "번째방의 값 : " + arr2[i]);
 		}
+
+		
 	}
 
 }
