4c4bc0cf4658a94f796fb7f60cbb295ebf7df581
ByeongUk-An <quddnr005@naver.com>
2022-03-28 23:31:08 +0900

    Thread

---
+++ src/Chap15/Faa.java
@@ -0,0 +1,18 @@
+package Chap15;
+
+class A {
+	int[] a = {1,2,3};
+	
+	
+}
+
+public class Faa {
+
+	public static void main(String[] args) {
+	A a = new A();
+	
+	System.out.println(a.a[3]);
+
+	}
+
+}
