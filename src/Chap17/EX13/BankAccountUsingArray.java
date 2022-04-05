d2f80bad6c095f6c76009e917e243af3d128055a
ByeongUk-An <quddnr005@naver.com>
2022-04-05 23:24:39 +0900

    bank

--- src/Chap17/EX13/BankAccountUsingArray.java
+++ src/Chap17/EX13/BankAccountUsingArray.java
@@ -1,5 +1,6 @@
 package Chap17.EX13;
 
+import java.util.Arrays;
 import java.util.Scanner;
 //완료시간 내일 아침9:30분까지
 
@@ -8,8 +9,10 @@ class Account {
 	private String owner; // 계좌 주, 이름
 	private int balance; // 통장금액
 
-	public Account(String ano2, String owner2, int balance2) {
-		// TODO Auto-generated constructor stub
+	public Account(String ano, String owner, int balance) {
+		this.ano = ano;
+		this.owner = owner;
+		this.balance = balance;
 	}
 
 	public String getAno() {
@@ -36,12 +39,17 @@ class Account {
 		this.balance = balance;
 	}
 
+//	public static Account findAccount(int i) {
+//		// TODO Auto-generated method stub
+//		return null;
+//	}
+
 }
 
 public class BankAccountUsingArray {
 	// 일반배열을 사용해서 Account 객체 생성
 	private static Account[] accountArray = new Account[100]; // 배열에 객체 저장.
-	private static int index = 1;
+	private static int index;
 	private static Scanner scanner = new Scanner(System.in);
 
 	private static void createAccount(Account account) {
@@ -51,32 +59,54 @@ public class BankAccountUsingArray {
 		System.out.println("계좌생성 중입니다.");
 		System.out.println("---------");
 		accountArray[index++] = account;
+		System.out.println(index +"번째 계좌가 생성이 되었습니다.");
+		
+		
+		
 	}
 
 	private static void accountList() {
 		// 코드 작성 : 2. 배열에 저장된 객체를 가져와서 계좌번호, 이름, 금액을 출력
-		System.out.println("------");
+		System.out.println("---------");
 		System.out.println("목록조회");
-		System.out.println("------");
-		for(int i = 0; i<index; i++) {
-			System.out.print(accountArray[i].getAno()+" ");
-            System.out.print(accountArray[i].getOwner()+" ");
-            System.out.println(accountArray[i].getBalance());
+		System.out.println("---------");
+
+		for (int i = 0; i < index; i++) {
+			
+			System.out.print(accountArray[i].getAno() + " ");
+			System.out.print(accountArray[i].getOwner() + " ");
+			System.out.println(accountArray[i].getBalance());
 		}
 	}
 
-	private static void deposit(String accountNo,int amount) {
+	private static void deposit(String accountNo, int balance) {
 		// 코드 작성 : 3. 예금 <== 계좌번호를 받아서 객체에서 해당 계좌를 찾아서, 입금금액을 받아서 해당계좌에 저장.
+		System.out.println("------");
+		System.out.println("예금이 되었습니다.");
+		System.out.println("------");
+		Account account = findAccount(accountNo);
+		account.setBalance(account.getBalance() + balance);
+		System.out.println("현재 잔액은" + account.getBalance()+"원 입니다.");
 	}
 
-	private static void withdraw() {
+	private static void withdraw(String accountNo, int balance) {
 		// 코드 작성 : 4. 출금 <== 계좌번호를 인풋 받아 해당 계좌를 객체에서 찾아서 출금.
+		System.out.println("---------");
+		System.out.println("출금 되었습니다.");
+		System.out.println("---------");
+		Account account = findAccount(accountNo);
+		account.setBalance(account.getBalance() - balance);
+		System.out.println("현재 잔액은" +account.getBalance()+"원 입니다.");
 	}
 
 	// 배열에서 Account 객체 내의 ano[계좌번호] 와 동일한 계좌를 찾아서 찾은 객체를 리턴으로 돌려줌.
 	private static Account findAccount(String ano) {
 		Account account = null;
-
+		for (int i = 0; i < index; i++) {
+			if (ano.equals(accountArray[i].getAno())) {
+				account = accountArray[i];
+			}
+		}
 		return account;
 	}
 
@@ -104,13 +134,17 @@ public class BankAccountUsingArray {
 				accountList();
 			} else if (selectNo == 3) {
 				System.out.print("계좌번호 입력 >> ");
-                String accountNo = scanner.next();
-                System.out.print("입금액 입력 >> ");
-                int balance = scanner.nextInt();
-                deposit(accountNo, balance);
-				
+				String accountNo = scanner.next();
+				System.out.print("입금액 입력 >> ");
+				int balance = scanner.nextInt();
+				deposit(accountNo, balance);
+
 			} else if (selectNo == 4) {
-				withdraw();
+				System.out.print("계좌번호 입력 >> ");
+				String accountNo = scanner.next();
+				System.out.print("출금액 입력 >> ");
+				int balance = scanner.nextInt();
+				withdraw(accountNo, balance);
 			} else if (selectNo == 5) {
 				run = false;
 				// break
